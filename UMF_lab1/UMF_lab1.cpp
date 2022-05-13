41998ee3b17a28616b396d8e76c7c3069cd8fb75
AlexandraGribova <sasha.gribova@bk.ru>
2022-05-13 23:01:39 +0700

    Добавьте файлы проекта.

---
+++ UMF_lab1/UMF_lab1.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include"Grid.h"
+#include"Sole.h"
+using namespace std;
+
+
+int main()
+{
+    Grid grid;
+    SOLE sole(grid);
+}
